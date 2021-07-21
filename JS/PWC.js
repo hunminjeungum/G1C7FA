cc59e3207e0016882d5b40118d8670ed78fb8894
SYH08 <yihyunsong@outlook.com>
2021-07-21 12:33:10 +0900

    Create PWC.js

---
+++ JS/PWC.js
@@ -0,0 +1,13 @@
+password = prompt("Student authentication");
+
+    if(password=='조성욱 최고'){
+
+      alert(':) Confirmed that you are a student with permission to view this site.');
+
+    }else{
+
+      alert(':( You do not have permission to view this site');
+      window.location.href='https://hunminjeungum.github.io/G1C2FA/Sub%20HTML/50rry.html';
+
+
+    }
