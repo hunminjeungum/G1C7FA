c3b764644ac400854da239169189c89c72fb5606
SYH08 <yihyunsong@outlook.com>
2021-08-04 20:58:29 +0900

    create src folder

--- src/JS/PWC.js
+++ src/JS/PWC.js
@@ -1,9 +1,7 @@
 password = prompt("Authentication");
 
-    if(password=='G1C7FA Beta')
+    if(password !='G1C7FA Beta')
     {
-
-    }else{
 
       alert(':( You do not have permission to view this site');
       window.location.href='https://hunminjeungum.github.io/G1C2FA/Sub%20HTML/50rry.html';
